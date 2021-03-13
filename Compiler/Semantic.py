54c0a6cb6eac778f6b9b47a1f2a3a90d7f34d270
dasome29 <dasome29@hotmail.com>
2021-03-13 13:26:50 -0600

    Add -Skeleton for Semantic Analysis.

---
+++ Compiler/Semantic.py
@@ -0,0 +1,79 @@
+class Element:
+    def __init__(self):
+        print("")
+
+
+class Function(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class VariableDef(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Put(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Add(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Continue(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Pos(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class UseColor(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Begin(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Speed(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Run(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Repeat(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class If(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Elif(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class Until(Element):
+    def __init__(self):
+        super().__init__()
+
+
+class While(Element):
+    def __init__(self):
+        super().__init__()
+
--- Compiler/Syntax.py
+++ Compiler/Syntax.py
@@ -1,1 +1,14 @@
 import yacc
+
+
+class Parser(object):
+    start = "program"
+
+    def __init__(self, lexer):
+        self.parser = None
+        self.lexer = lexer
+        self.tokens = self.lexer.tokens
+        self.parser = yacc.yacc(module=self, start=self.start)
+
+    def parse(self, data):
+        self.parser.parse(data, self.lexer)
