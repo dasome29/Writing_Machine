632174c6cc1de82e36a8779b2f4a060aea097321
dasome29 <dasome29@hotmail.com>
2021-03-13 16:15:02 -0600

    Add -Basic structure for expressions in Syntax.py

--- Compiler/Syntax.py
+++ Compiler/Syntax.py
@@ -96,92 +96,154 @@ class Parser(object):
         pass
 
     def p_variable_def(self, p):
-        """variable_def : Def ID EQUALTHAN expression SEMICOLON"""
+        """variable_def : DEF ID EQUALTHAN expression SEMICOLON"""
         p[0] = VariableDef()
         pass
 
     def p_put(self, p):
-        """put : Put ID EQUALTHAN expression SEMICOLON"""
+        """put : PUT ID EQUALTHAN expression SEMICOLON"""
         p[0] = Put()
         pass
 
     def p_add(self, p):
-        """add : Add LBRACKET ID COMMA expression RBRACKET SEMICOLON"""
+        """add : ADD LBRACKET ID COMMA expression RBRACKET SEMICOLON"""
         p[0] = Add()
 
     def p_add_one(self, p):
-        """add : Add LBRACKET ID RBRACKET SEMICOLON"""
+        """add : ADD LBRACKET ID RBRACKET SEMICOLON"""
         p[0] = Add()
 
     def p_continue(self, p):
-        """continue : ContinueUp expression SEMICOLON
-            | ContinueDown expression SEMICOLON
-            | ContinueRight expression SEMICOLON
-            | ContinueLeft expression SEMICOLON"""
+        """continue : CONTINUEUP expression SEMICOLON
+            | CONTINUEDOWN expression SEMICOLON
+            | CONTINUERIGHT expression SEMICOLON
+            | CONTINUELEFT expression SEMICOLON"""
         p[0] = Continue()
+        pass
 
     def p_pos(self, p):
-        """pos : Pos LBRACKET expression COMMA expression RBRACKET SEMICOLON"""
+        """pos : POS LBRACKET expression COMMA expression RBRACKET SEMICOLON"""
         p[0] = Pos()
         pass
 
     def p_posX(self, p):
-        """pos : PosX expression SEMICOLON"""
+        """pos : POSX expression SEMICOLON"""
         p[0] = Pos()
         pass
 
     def p_posY(self, p):
-        """pos : PosY expression SEMICOLON"""
+        """pos : POSY expression SEMICOLON"""
         p[0] = Pos()
         pass
 
     def p_useColor(self, p):
-        """usecolor : UseColor expression SEMICOLON"""
+        """usecolor : USECOLOR expression SEMICOLON"""
         p[0] = UseColor()
         pass
 
     def p_elevation(self, p):
-        """elevation : Up SEMICOLON
-                     | Down SEMICOLON"""
+        """elevation : UP SEMICOLON
+                     | DOWN SEMICOLON"""
         p[0] = Elevation()
         pass
 
     def p_begin(self, p):
-        """begin : Begin SEMICOLON"""
+        """begin : BEGIN SEMICOLON"""
         p[0] = Begin()
         pass
 
     def p_speed(self, p):
-        """speed : Speed expression SEMICOLON"""
+        """speed : SPEED expression SEMICOLON"""
         p[0] = Speed()
         pass
 
     def p_run(self, p):
-        """run : Run LBRACKET compound_procedure RBRACKET SEMICOLON"""
+        """run : RUN LBRACKET compound_procedure RBRACKET SEMICOLON"""
         p[0] = Run()
         pass
 
     def p_repeat(self, p):
-        """repeat : Repeat expression LBRACKET compound_procedure RBRACKET SEMICOLON"""
+        """repeat : REPEAT expression LBRACKET compound_procedure RBRACKET SEMICOLON"""
         p[0] = Repeat()
         pass
 
     def p_if(self, p):
-        """if : If condition LBRACKET compound_procedure RBRACKET SEMICOLON"""
+        """if : IF condition LBRACKET compound_procedure RBRACKET SEMICOLON"""
         p[0] = If()
         pass
 
     def p_elif(self, p):
-        """elif : Elif condition LBRACKET compound_procedure RBRACKET LBRACKET compound_procedure RBRACKET SEMICOLON"""
+        """elif : ELIF condition LBRACKET compound_procedure RBRACKET LBRACKET compound_procedure RBRACKET SEMICOLON"""
         p[0] = Elif()
         pass
 
     def p_until(self, p):
-        """until : Until LBRACKET compound_procedure RBRACKET LBRACKET condition RBRACKET SEMICOLON"""
+        """until : UNTIL LBRACKET compound_procedure RBRACKET LBRACKET condition RBRACKET SEMICOLON"""
         p[0] = Until()
         pass
 
     def p_while(self, p):
-        """while : While LBRACKET condition RBRACKET LBRACKET compound_procedure RBRACKET SEMICOLON"""
+        """while : WHILE LBRACKET condition RBRACKET LBRACKET compound_procedure RBRACKET SEMICOLON"""
         p[0] = While()
         pass
+
+    def p_condition(self, p):
+        """condition : LPAREN condition RPAREN"""
+        p[0] = p[2]
+        pass
+
+    def p_condition_operator(self, p):
+        """condition : condition EQUALTHAN condition
+                     | condition GREATERTHAN condition
+                     | condition LESSTHAN condition
+                     | expression"""
+        try:
+            if p[2] == "==":
+                p[0] = p[1] == p[3]
+            elif p[2] == ">":
+                p[0] = p[1] > p[3]
+            elif p[2] == "<":
+                p[0] = p[1] < p[3]
+        except IndexError:
+            p[0] = p[1]
+        pass
+
+    def p_boolean(self, p):
+        """boolean : AND LPAREN expression COMMA expression RPAREN SEMICOLON
+                   | OR LPAREN expression COMMA expression RPAREN SEMICOLON
+                   | GREATER LPAREN expression COMMA expression RPAREN SEMICOLON
+                   | SMALLER LPAREN expression COMMA expression RPAREN SEMICOLON
+                   | EQUAL LPAREN expression COMMA expression RPAREN SEMICOLON"""
+        if p[1] == "And":
+            p[0] = p[3] and p[5]
+        elif p[1] == "Or":
+            p[0] = p[3] or p[5]
+        elif p[1] == "Greater":
+            p[0] = p[3] > p[5]
+        elif p[1] == "Less":
+            p[0] = p[3] < p[5]
+        elif p[1] == "Equal":
+            p[0] = p[3] == p[5]
+        pass
+
+    def p_boolean_not(self, p):
+        """boolean : NOT LPAREN expression RPAREN SEMICOLON"""
+        p[0] = not p[3]
+        pass
+
+    def p_arithmetic(self, p):
+        """arithmetic : MULTIPLY LPAREN expression COMMA expression RPAREN SEMICOLON
+                      | DIVIDE LPAREN expression COMMA expression RPAREN SEMICOLON
+                      | POWER LPAREN expression COMMA expression RPAREN SEMICOLON
+                      | ADDITION LPAREN expression COMMA expression RPAREN SEMICOLON
+                      | SUBTRACT LPAREN expression COMMA expression RPAREN SEMICOLON"""
+        if p[1] == "Multiply":
+            p[0] = p[3] * p[5]
+        elif p[1] == "Divide":
+            p[0] = p[3] // p[5]
+        elif p[1] == "Power":
+            p[0] = p[3] ** p[5]
+        elif p[1] == "Addition":
+            p[0] = p[3] + p[5]
+        elif p[1] == "Subtract":
+            p[0] = p[3] - p[5]
