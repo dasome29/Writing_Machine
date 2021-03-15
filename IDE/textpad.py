eb071e2b83baee88bcd79e6c4d6198318edadb56
miguemesen <miguemesen12@gmail.com>
2021-03-15 04:28:23 -0600

    Added addfiles and saveasfiles

--- IDE/textpad.py
+++ IDE/textpad.py
@@ -6,6 +6,51 @@ root = Tk()
 root.title('My Title')
 root.geometry('1200x660')
 
+def newFile():
+    # Delete previous text
+    myText.delete("1.0", END)
+    # Update status bars
+    root.title('New File')
+    statusBar.config(text="New File")
+
+# Save as File
+def save_as_file():
+    textFile = filedialog.asksaveasfilename(defaultextension=".*",initialdir="/Users/migue/Desktop/files_ide", title="Save File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+    if textFile:
+        # Update status bars
+        name = textFile
+        statusBar.config(text="Saved: " +name)
+        name = name.replace("/Users/migue/Desktop/files_ide/", "")
+        root.title(name)
+
+        # Save the file
+        textFile = open(textFile, 'w')
+        textFile.write(myText.get(1.0, END))
+        # Close the file
+        textFile.close()
+
+def openFile():
+    # Delete previous text
+    myText.delete("1.0", END)
+
+    # Grab Filename
+    textFile = filedialog.askopenfilename(initialdir="/Users/migue/Desktop/files_ide", title="Open File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+    
+    # Update status bars
+    name = textFile
+    statusBar.config(text=name)
+    name = name.replace("/Users/migue/Desktop/files_ide/", "")
+    root.title(name)
+    
+    # Open the file
+    textFile = open(textFile, 'r')
+    stuff = textFile.read()
+    # Add file to textbox
+    myText.insert(END, stuff)
+    # Close the opend file
+    textFile.close()
+
+
 # Create Main Frame
 myFrame = Frame(root)
 myFrame.pack(pady=5)
@@ -28,9 +73,10 @@ root.config(menu=myMenu)
 # Add File Menu
 fileMenu = Menu(myMenu, tearoff=False)
 myMenu.add_cascade(label="File", menu=fileMenu)
-fileMenu.add_command(label="New")
-fileMenu.add_command(label="Open")
+fileMenu.add_command(label="New", command=newFile)
+fileMenu.add_command(label="Open", command=openFile)
 fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Save As", command=save_as_file)
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=root.quit)
 
@@ -44,7 +90,7 @@ editMenu.add_command(label="Undo")
 editMenu.add_command(label="Redo")
 
 # Add Status Bar to Bottom of App
-statusBar = Label(root, text='Ready     ', anchor=E)
+statusBar = Label(root, text='Ready', anchor=E)
 statusBar.pack(fill=X, side= BOTTOM, ipady=5)
 
 
